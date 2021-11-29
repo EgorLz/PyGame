c287af7628431d65be9203f5a58a4c3eb8caae9b
Egor <mikhaylov2013a@yandex.ru>
2021-11-29 18:50:59 +0300

    Navi

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 # https://github.com/EgorLz/PyGame.git
 import pygame
 
+def draw(screen):
+    screen.fill('yellow')
+    font = pygame.font.Font(None, 200)
+    text = font.render('NAVI', True, 'black')
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2
+    screen.blit(text, (text_x, text_y))
+
+
 if __name__ == '__main__':
     pygame.init()
     size = width, height = 800, 600
     screen = pygame.display.set_mode(size)
+
+    draw(screen)
 
     pygame.display.flip()
 
