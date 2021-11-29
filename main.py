f8099084b8df772ba2555ad2f7b11e883e87c563
Egor <mikhaylov2013a@yandex.ru>
2021-11-29 18:33:11 +0300

    gggg

---
+++ main.py
@@ -0,0 +1,14 @@
+# https://github.com/EgorLz/PyGame.git
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+
+    pygame.display.flip()
+
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+
+    pygame.quit()
